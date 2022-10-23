6591aeaf28a32b4e8561feddf8d7c050800068ff
michael <y.t@posteo.jp>
2022-10-23 21:14:40 +0300

    init 2

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -3,9 +3,21 @@
 pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
-
 contract  WavePortal {
+
+    uint256 totalWaves;
+
     constructor() {
         console.log("yo smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("we have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
